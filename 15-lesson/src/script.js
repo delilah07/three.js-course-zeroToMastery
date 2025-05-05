f035f7c4ea4542d7aa7a4ed975eed420e55c6705
Iryna Abrosimova <iryna.abrosimova@cxdisruption.com>
2025-05-05 10:34:55 +0200

    add earth and moon anim

--- 15-lesson/src/script.js
+++ 15-lesson/src/script.js
@@ -55,11 +55,19 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
+// initialize a clock
+const clock = new THREE.Clock();
+
 // render loop
 const renderloop = () => {
+  const elapsedTime = clock.getElapsedTime();
   //add animation
   earth.rotation.y += 0.01;
-  sun.rotation.y += 0.005;
+  earth.position.x = 10 * Math.sin(elapsedTime);
+  earth.position.z = 10 * Math.cos(elapsedTime);
+
+  moon.position.x = 2 * Math.sin(elapsedTime);
+  moon.position.z = 2 * Math.cos(elapsedTime);
 
   controls.update();
   renderer.render(scene, camera);
